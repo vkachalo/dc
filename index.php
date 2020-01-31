16cec57a7be739e59e7299a3d28c080937df5510
Vasiliy Kachalo <vasiliy.kachalo@gmail.com>
2020-01-31 20:12:12 +0200

    Shop is OK!

--- index.php
+++ index.php
@@ -1,50 +1,19 @@
-<html>
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=win-1251">
-1111111
-</head>
-<body>
 <?php
+// Version
+define('VERSION', '3.0.3.2');
 
-function get_categories()
-{
+// Configuration
+if (is_file('config.php')) {
+	require_once('config.php');
+}
 
-	$url = 'https://api.dclink.com.ua/api/GetCategories';
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($ch, CURLOPT_POST, 1);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, array ('login' => 'L_gerasimoff(КПД)', 'password' => 'w5v6C2E2r1', 'доппараметр' => ''));
-	curl_setopt($ch, CURLOPT_URL, $url);
-	$output = curl_exec($ch);
-	curl_close($ch);
+// Install
+if (!defined('DIR_APPLICATION')) {
+	header('Location: install/index.php');
+	exit;
+}
 
-	$catslist = new SimpleXMLElement($output);
-	foreach ($catslist->Category as $category)
-	{
-		echo '['.$category->CategoryID.'] '.$category->CategoryName.' ['.$category->ParentID.']<br>';
-	}
-}	
+// Startup
+require_once(DIR_SYSTEM . 'startup.php');
 
-function get_items($categoryid)
-{
-	$url = 'https://api.dclink.com.ua/api/GetPrice';
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($ch, CURLOPT_POST, 1);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, array ('login' => 'L_gerasimoff(КПД)', 'password' => 'w5v6C2E2r1', 'доппараметр' => ''));
-	curl_setopt($ch, CURLOPT_URL, $url);
-	$output = curl_exec($ch);
-	curl_close($ch);
-
-	echo $output;
-}	
-	
-
-	get_items(1);
-?>
-</body>
-</html>
+start('catalog');
